be5ac51ca786d8df56a901b02a8bda341b2f70b4
samuel.mahembe <samuel.mahembe@econet.co.zw>
2023-11-11 11:01:00 +0200

    implemented method to execute initialised blocks

--- src/buildingblocks/BuildingBlocks.java
+++ src/buildingblocks/BuildingBlocks.java
@@ -1,6 +1,15 @@
 package buildingblocks;
 
 public class BuildingBlocks {
+    {
+        System.out.println("We are in class file first");
+    }
+    public BuildingBlocks() {
+        {
+            System.out.println("We are in class constructor");
+        }
+    }
+
     public static void main(String [] args){
         {
             BuildingBlocks buildingBlocks= new BuildingBlocks();
@@ -9,6 +18,6 @@ public class BuildingBlocks {
         }
     }
     {
-        System.out.println("We are in class file");
+        System.out.println("We are in class file  last");
     }
 }
