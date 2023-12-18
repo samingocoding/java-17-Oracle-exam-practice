e282a02e691374bb796946332ce36cb13fd1a687
samuel.mahembe <samuel.mahembe@econet.co.zw>
2023-12-18 19:36:05 +0200

    implemented introductory topic of pattern Matching in java 16

---
+++ src/makingdecisions/PatternMatching.java
@@ -0,0 +1,38 @@
+package makingdecisions;
+
+import java.util.List;
+
+public class PatternMatching {
+    public static void main(String[] args) {
+        System.out.println("Rule: if output 0 = match  output 1 = value above output =-1 value below ");
+        System.out.println("Checking if number is equal to 5");
+        getIfNumberEqualTo5(2);
+        System.out.println("Checking if number is > 5");
+        getIfNumberGreaterThan5(8);
+
+        Number value = 123;
+        if(value instanceof List) {}
+        if(value instanceof List data) {
+            System.out.println(data);
+        }
+    }
+
+    private static void getIfNumberGreaterThan5(Number number) {
+        if (number instanceof Integer data && data.compareTo(5)>0){
+            System.out.println(data);
+        }
+    }
+
+    private static void getIfNumberEqualTo5(Number number){
+        if (number instanceof Integer data){
+            System.out.println(data.compareTo(5));
+        }
+    }
+
+    void printOnlyIntegers(Number number) {
+        if (!(number instanceof Integer data)){
+            return;
+        }
+        System.out.print(data.intValue());
+    }
+}
