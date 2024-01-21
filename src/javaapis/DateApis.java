48cdfb1d03a1f175237dc192722442df846e8bb1
samuel.mahembe <samuel.mahembe@econet.co.zw>
2024-01-21 22:49:37 +0200

    implemented java 17 date apis for Duration and Period Classes

--- src/javaapis/DateApis.java
+++ src/javaapis/DateApis.java
@@ -1,6 +1,7 @@
 package javaapis;
 
 import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public class DateApis {
     public static void main(String[] args) {
@@ -16,5 +17,21 @@ public class DateApis {
         Period weekly = Period.ofWeeks(3);
         LocalDate dateBorn = LocalDate.of(1999,6,24);
         System.out.println(weekly);
+        Period monthly = Period.ofMonths(2);
+        System.out.println(monthly);
+        Period yearly = Period.ofYears(3);
+        System.out.println(yearly);
+        System.out.println(dateBorn.plus(monthly));;
+
+        /***
+         * Working with Duration
+         * Used to store Time  that is days, hours, minuets,
+         * */
+        var one = LocalTime.of(5, 15);
+        var two = LocalTime.of(6, 30);
+        var date = LocalDate.of(2016, 1, 20);
+        System.out.println(ChronoUnit.HOURS.between(one, two));
+        System.out.println(ChronoUnit.MINUTES.between(one, two));// NB order matters (.between(x,y))  (y-x)
+       // System.out.println(ChronoUnit.MINUTES.between(one, date));
     }
 }
