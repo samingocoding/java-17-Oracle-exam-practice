200a5c4efadb0b10bc9afbfd50f18b0390de2d43
samuel.mahembe <samuel.mahembe@econet.co.zw>
2024-01-23 20:59:39 +0200

    implemented java 17 date apis for Duration and Period Classes

--- src/javaapis/DateApis.java
+++ src/javaapis/DateApis.java
@@ -33,5 +33,33 @@ public class DateApis {
         System.out.println(ChronoUnit.HOURS.between(one, two));
         System.out.println(ChronoUnit.MINUTES.between(one, two));// NB order matters (.between(x,y))  (y-x)
        // System.out.println(ChronoUnit.MINUTES.between(one, date));
+
+        /****
+         * Working with Instant
+         * Used to hold Zoned time moment
+         */
+        var date2 = LocalDate.of(2022, 5, 25);
+        var time = LocalTime.of(11, 55, 00);
+        var zone = ZoneId.of("US/Eastern");
+        var zonedDateTime = ZonedDateTime.of(date2, time, zone);
+        var instant = zonedDateTime.toInstant(); // 2022–05–25T15:55:00Z
+        System.out.println(zonedDateTime); // 2022–05–25T11:55–04:00[US/Eastern]
+        System.out.println(instant); // 202–05–25T15:55:00Z
+        var testingLocDT = LocalDateTime.of(date2,time);
+        System.out.println(testingLocDT);
+
+        /**
+         * Trying DayLight Saving Time
+         * ***/
+        System.out.println("Trying DayLight Saving Time");
+        var date3 = LocalDate.of(2022, Month.MARCH, 6);
+        var time4 = LocalTime.of(1, 30);
+        var zone4 = ZoneId.of("US/Eastern");
+        var dateTime = ZonedDateTime.of(date3, time4, zone4);
+        System.out.println(dateTime); // 2022-11-06T01:30-04:00[US/Eastern]
+        dateTime = dateTime.plusHours(1);
+        System.out.println(dateTime); // 2022-11-06T01:30-05:00[US/Eastern]
+        dateTime = dateTime.plusHours(1);
+        System.out.println(dateTime); // 2022-11-06T02:3
     }
 }
