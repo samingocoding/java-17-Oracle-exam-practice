ba3153f1d1e6d30823f3438055e933aca2bb89af
samuel.mahembe <samuel.mahembe@econet.co.zw>
2024-03-08 23:48:01 +0200

    init commit

--- src/buildingblocks/DataTypes.java
+++ src/buildingblocks/DataTypes.java
@@ -1,10 +1,22 @@
 package buildingblocks;
 
 public class DataTypes {
+    private int faw;
     public static void main(String[] args) {
         //helper  methods of refrence types
 
         Integer samReference = Integer.valueOf(9);
         int sam  = samReference.intValue();
+        int pam;
+
+        pam =5;
+        System.out.println(pam);
+
+        short small = 4+9;
+        int lag= 5;
+        int ret = 6;
+      //  short cast = lag+ret;
+        
+
     }
 }
---
+++ src/buildingblocks/TextBlocks.java
@@ -0,0 +1,10 @@
+package buildingblocks;
+
+public class TextBlocks {
+    private int numForks;
+    public static void main(String[] args) { int numKnives;
+        System.out.print("""
+"# forks = " + numForks +
+" # knives = " + numKnives +
+# cups = 0"""); }
+}
